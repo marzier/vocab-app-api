a6573d438fe169d98f005c172c7e591178e31a52
“marzier” <atton.rand.23@gmail.com>
2020-03-31 13:41:56 -0700

    knexfile prod object update to match lsnotes for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -51,7 +51,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
     },
     seeds: {
       directory: './data/seeds',
